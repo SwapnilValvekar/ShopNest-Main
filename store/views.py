ef2d58de294d31d2ac73d7e2fd33ff1bdff3ff24
Swapnil Valvekar <swapnil.valvekar@gmail.com>
2023-10-12 20:22:18 +0530

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,237 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import json
+import datetime
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .models import *
+
+
+# from .utils import cartData
+
+
+def cookieCart(request):
+    # Create empty cart for now for non-logged in user
+    try:
+        cart = json.loads(request.COOKIES['cart'])
+    except:
+        cart = {}
+        print('CART:', cart)
+
+    items = []
+    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+    cartItems = order['get_cart_items']
+
+    for i in cart:
+        # We use try block to prevent items in cart that may have been removed from causing error
+        try:
+            if (cart[i]['quantity'] > 0):  # items with negative quantity = lot of freebies
+                cartItems += cart[i]['quantity']
+
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
+
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
+
+                item = {
+                    'id': product.id,
+                    'product': {'id': product.id, 'name': product.name, 'price': product.price,
+                                'imageURL': product.imageURL}, 'quantity': cart[i]['quantity'],
+                    'digital': product.digital, 'get_total': total,
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+        except:
+            pass
+
+    return {'cartItems': cartItems, 'order': order, 'items': items}
+
+
+def cartData(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
+    return {'cartItems': cartItems, 'order': order, 'items': items}
+
+
+def store(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    products = Product.objects.all()
+
+    # Check if the user is authenticated and get the username
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+
+    context = {'products': products, 'cartItems': cartItems, 'username': username}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    return JsonResponse('Payment submitted..', safe=False)
+
+
+from django.contrib.auth.models import User
+from .models import Customer
+
+# def signup_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             # confirm_password = form.cleaned_data.get('confirm_password')
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 Customer.objects.create(user=user)
+#                 return redirect('/login')  
+
+#         else:
+#             messages.error(request, 'Invalid form data')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'store/signup.html', {'form': form})
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Customer
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if username and password and password == confirm_password:
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+
+                    Customer.objects.create(user=user)
+                    return redirect('/login')
+            else:
+                messages.error(request, 'Username already exists.')
+        else:
+            messages.error(request, 'Invalid form data.')
+
+    return render(request, 'store/signup.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            try:
+                customer = request.user.customer
+            except Customer.DoesNotExist:
+                Customer.objects.create(user=request.user)
+            return redirect('/store')
+        else:
+            messages.error(request, 'Invalid credentials')
+            messages.warning(request, "ENTER CORRECT CREDENTIALS")
+    return render(request, 'store/login.html')
